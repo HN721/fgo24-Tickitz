0566300cb4a8dcd32938a066092f97b9fc05594c
HN721 <hosea1422@gmail.com>
2025-05-21 20:35:01 +0700

    feat: create function generate Token

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,5 +1,7 @@
 import Swal from "sweetalert2";
-
+function generateFakeToken() {
+  return Math.random().toString(36).substr(2) + Date.now().toString(36);
+}
 export function useLogin({ email, password }) {
   const users = JSON.parse(localStorage.getItem("Auth") || "[]");
 
@@ -20,6 +22,19 @@ export function useLogin({ email, password }) {
       text: "Wrong Password",
     });
   }
+  const tokenData = {
+    token: generateFakeToken(),
+    user: {
+      id: foundUser.id,
+      username: foundUser.username,
+      email: foundUser.email,
+      phone: foundUser.phone,
+    },
+    createdAt: new Date().toISOString(),
+    expiredAt: new Date(Date.now() + 60 * 60 * 1000).toISOString(),
+  };
+
+  localStorage.setItem("token", JSON.stringify(tokenData));
 
   return true;
 }
