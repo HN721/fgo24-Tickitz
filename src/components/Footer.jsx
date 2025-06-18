cd6be2486b61a3d24cbf7efe32d85c23dc53e874
HN721 <hosea1422@gmail.com>
2025-06-18 22:41:33 +0700

    feat: update animation on title and change logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/footer.png";
+import logo from "../assets/mv-logos.png";
 import Ebu from "../assets/ebu.svg";
 import CineOne from "../assets/cineOne.svg";
 import Hiflix from "../assets/hiflix.svg";
